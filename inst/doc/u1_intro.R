86e5996fe2ade68696572564746b27f48a83e8fb
Brodie Gaslam <brodie.gaslam@yahoo.com>
2025-03-19 00:40:02 +0000

    version 1.4.22

--- inst/doc/u1_intro.R
+++ inst/doc/u1_intro.R
@@ -1,21 +1,21 @@
 ## ----eval=FALSE---------------------------------------------------------------
-#  install.packages('unitizer')
+# install.packages('unitizer')
 
 ## ----eval=FALSE---------------------------------------------------------------
-#  library(unitizer)
-#  demo(unitizer)
+# library(unitizer)
+# demo(unitizer)
 
 ## ----eval=FALSE---------------------------------------------------------------
-#  num.var <- 14523.2342520  # assignments are not considered tests
-#  test_me(num.var)          # safe
+# num.var <- 14523.2342520  # assignments are not considered tests
+# test_me(num.var)          # safe
 
 ## ----eval=FALSE---------------------------------------------------------------
-#  test_me(14523.2342520)    # could be deparsed differently
+# test_me(14523.2342520)    # could be deparsed differently
 
 ## ----eval=FALSE---------------------------------------------------------------
-#  chr <- "hello\u044F"      # assignments are not considered tests
-#  fun_to_test(chr)          # safe
+# chr <- "hello\u044F"      # assignments are not considered tests
+# fun_to_test(chr)          # safe
 
 ## ----eval=FALSE---------------------------------------------------------------
-#  fun_to_test("hello\u044F") # could be deparsed differently
+# fun_to_test("hello\u044F") # could be deparsed differently
 
